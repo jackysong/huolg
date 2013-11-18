9a7317bc00eed9b1586f179cb31a41e726c4f025
jackysong <songzibin123@126.com>
2013-11-18 23:29:59 +0800

    安装完

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '106.186.114.166';
+$cfg_dbname = 'huolg603';
+$cfg_dbuser = 'root';
+$cfg_dbpwd = 'q1w2e3r4';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
