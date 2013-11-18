a333e2703434d10b72ab125e66c20dda3f6f9244
jackysong <songzibin123@126.com>
2013-11-18 23:47:12 +0800

    Update common.inc.php

--- data/common.inc.php
+++ data/common.inc.php
@@ -1,11 +1,9 @@
 <?php
 //数据库连接信息
-$cfg_dbhost = '106.186.114.166';
-$cfg_dbname = 'huolg603';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = 'q1w2e3r4';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'test';
+$cfg_dbuser = 'test';
+$cfg_dbpwd = 'test';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
-
-
 ?>
